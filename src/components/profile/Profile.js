6177265aad5ae3fa586cc06be7f5f48089b69c10
winded01 <windedvirus03@mail.ru>
2023-07-31 19:12:55 +0900

    add section profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import './profile.css'
+import './profile.css';
+import avatar from '../../img/Elon_mask.jpg';
+import avatar_mini from '../../img/Elon_Mask_small.jpg';
 function Profile() {
   return (
- <div className='profile'>profile</div>
+ <div className='profile'>
+  <div className='user'>
+    <img src={avatar_mini} />
+    <p>Илон Маск</p>
+  </div>
+  <div className='posts'>
+   <input placeholder='введите текст' />
+   <button>добавить пост</button>
+   <div className='post'>
+     <img src={avatar_mini} />
+     <span>Илон Маск</span>
+     <p>Hello, world</p>
+     
+   </div>
+  </div>
+ </div>
   );
 }
 
