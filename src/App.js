ee62d503e7eccb8d5ec2b48e30efc5567ce14df6
winded01 <windedvirus03@mail.ru>
2023-08-23 20:10:03 +0900

    add router in app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
 import Profile from './components/profile/Profile';
+import Dialogs from './components/dialogs/Dialog';
+import { BrowserRouter, Route, Routes  } from 'react-router-dom';
 function App() {
   return (
  <div className='wrapper'>
+  <BrowserRouter>
   <Header /> 
   <Navbar />
-  <Profile />
+  <Routes>
+  <Route exact path='/profile' Component={Profile}/>
+  <Route exact path='/dialog' Component={Dialogs}/>
+  </Routes>
+  </BrowserRouter>
  </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './navbar.css'
+import './navbar.css';
+import { NavLink } from 'react-router-dom';
 function Navbar() {
   return (
  <div className='navbar'>
-    <a>Profile</a>
-    <a>Messages</a>
-    <a>Users</a>
+    <NavLink to='/profile'>Profile</NavLink>
+    <NavLink to='/messages'>Messages</NavLink>
+    <NavLink to='/users'>Users</NavLink>
+    <NavLink to='/dialog'>Dialogs</NavLink>
  </div>
   );
 }
