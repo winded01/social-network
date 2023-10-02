1b1291d49c91dd7c654d13de63771f85bbc7a2e5
winded01 <windedvirus03@mail.ru>
2023-10-02 19:10:57 +0900

    store object

--- src/data/state.js
+++ src/data/state.js
@@ -1,64 +1,74 @@
-import { render } from '@testing-library/react';
 import avatar from '../img/Elon_mask.jpg';
-import renderTree from '../render';
-let state={
-   profilePage:{
-    postsData: [
-        {text:'hello world', id:'1', likes:8},
-        {text:'hello', id:'2', likes:3},
-        {text:'im Elon', id:'3', likes:5}
-    ],
-    newPostText:""
-   },
-   dialogsPage:{
-    dialogsData:[
-        {name:'Иван Иванов', id:1},
-        {name:'Дональд Трамп', id:2},
-        {name:'Билл Гейтс', id:3}
-    ],
-    messagesData:[
-        {message:'Hello world',id:1},
-        {message:'Hello',id:2},
-        {message:'Hi',id:3}
 
-    ],
-    newMessageText:""
+let store={
+   _state:{
+      profilePage:{
+       postsData: [
+           {text:'hello world', id:'1', likes:8},
+           {text:'hello', id:'2', likes:3},
+           {text:'im Elon', id:'3', likes:5}
+       ],
+       newPostText:""
+      },
+      dialogsPage:{
+       dialogsData:[
+           {name:'Иван Иванов', id:1},
+           {name:'Дональд Трамп', id:2},
+           {name:'Билл Гейтс', id:3}
+       ],
+       messagesData:[
+           {message:'Hello world',id:1},
+           {message:'Hello',id:2},
+           {message:'Hi',id:3}
+   
+       ],
+       newMessageText:""
+      },
+      friendsPage:{
+       friendsData:[
+           {name:'Вася', avatar:avatar, id:1},
+           {name:'Петя', avatar:avatar, id:2},
+           {name:'Иван', avatar:avatar, id:3}
+   
+       ]
+      }
    },
-   friendsPage:{
-    friendsData:[
-        {name:'Вася', avatar:avatar, id:1},
-        {name:'Петя', avatar:avatar, id:2},
-        {name:'Иван', avatar:avatar, id:3}
+   getState(){
+      return this._state;
+   },
 
-    ]
-   }
-};
-export default state;
-export let addPost=(postText)=>{
-    let newPost={
-        text:postText,
-        id:4,
-        likes:0
+   addPost(postText){
+       let newPost={
+           text:postText,
+           id:4,
+           likes:0
+       }
+     this._state.profilePage.postsData.unshift(newPost);
+    this._state.profilePage.newPostText="";
+    this.renderTree(this._state);
+   },
+    sendMessage (message){
+        let newMessage={
+           message:message,
+           id:4
+        }
+        this._state.dialogsPage.messagesData.push(newMessage);
+        this._state.dialogsPage.newMessageText="";
+        this.renderTree( this._state);
+   },
+    onPostChange (text){
+      this._state.profilePage.newPostText=text;
+      this.renderTree( this._state);
+    },
+     onMessageChange (text){
+      this._state.dialogsPage.newMessageText=text;
+      this.renderTree( this._state);
+    },
+    renderTree(){
+       console.log("fake function");
+    },
+     subscribe(observer){
+       this.renderTree=observer;
     }
- state.profilePage.postsData.unshift(newPost);
- state.profilePage.newPostText="";
- renderTree(state);
-// console.log(state);
 }
-export let sendMessage=(message)=>{
-     let newMessage={
-        message:message,
-        id:4
-     }
-     state.dialogsPage.messagesData.push(newMessage);
-     state.dialogsPage.newMessageText="";
-     renderTree(state);
-}
- export let onPostChange=(text)=>{
-    state.profilePage.newPostText=text;
-    renderTree(state);
- }
- export let onMessageChange=(text)=>{
-    state.dialogsPage.newMessageText=text;
-    renderTree(state);
- }
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,84 @@
-import renderTree from "./render.js";
-import state from "./data/state.js";
-renderTree(state);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import store from "./data/state"
+const root = ReactDOM.createRoot(document.getElementById('root'));
+let renderTree=(state)=>{
+  root.render(
+    <React.StrictMode>
+      <App  state={state} addPost={store.addPost.bind(store)} sendMessage={store.sendMessage.bind(store)} onPostChange={store.onPostChange.bind(store)} onMessageChange={store.onMessageChange.bind(store)}/>
+    </React.StrictMode>
+  );
+}
+
+renderTree(store.getState());
+store.subscribe(renderTree);
+export default renderTree;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+/*
+let person={//это объект , его можно представить как коробочку в котором храниться много данных в виде пары ключ: значение
+  _name:"Hex",//name - это ключ, по которому я могу найти значение - "Hex", таким образом  - person.name
+  _age :"15",// то же самое что иname
+  getName(){
+return this._name;
+  },
+  setName(name){
+    this._name=name;
+  }
+}
+person.setName("Dae");
+console.log(person.getName())
+
+let animal={
+  _type:"",
+  _name:"",
+  set(type){
+    this._type=type;
+  },
+  setName(name){
+    this._name=name;
+  },
+  getName(){
+    return this._name;
+  },
+  getType(){
+    return this._type;
+  }
+}
+animal.setName("Dima");
+console.log(animal.getName())
+animal.set("Lion");
+console.log(animal.getType())
+
+let car={
+  _model:"",
+  _year:"",
+  _color:"",
+  setProperties(model,year,color){
+    this._model=model;
+    this._year=year;
+    this._color=color;
+  },
+  getModel(){return this._model},
+  getYear(){return this._year},
+  getColor(){return this._color}
+}
+car.setProperties("BMW", "2009", "green")
+console.log(car.getModel() ,car.getYear(), car.getColor())
+
+
+//Отличие переменной от свойств объекта
+let name="Ned";// ПЕРЕМЕННАЯ  - поименованное  место в памяти где храним одно значение, чтобы сохранить что-то в переменной нужно применить операцию присваивания =
+person.name="Иван" // Это свойство объекта, которое так же как и переменная хранит одно значение, но доступ мы можем получить только через имя обЪекта.
+
+//Отличие функций от методов
+function SayName(){//функция создается отдельно, т.е ВНЕ класса или объекта, функцию мы вызываем просто указа ее имя - SayName()
+  console.log("Ivan")
+}
+//метод- это функция,котороая создается ВНУТРИ класса или объекта. Иными словами они пренадлежит объекту см. пример на 30 строке. метод мы вызываем только обратившись к объекту - person.sayName(); 
+*/
